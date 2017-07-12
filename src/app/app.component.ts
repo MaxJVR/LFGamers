c3602f3c0b421e35735c6ba29d78a20c351d3ba5
MaxJVR <MaxJVR@gmail.com>
2017-07-12 13:14:49 -0700

    connect firebase DB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,31 @@ import { AppComponent } from './app.component';
 
 import { AngularFireModule } from 'angularfire2';
 
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA6m_Uo6vxDQr4vZJwbagsltEoztVaJx4k',
+  authDomain: 'lfgamers-4ade1.firebaseapp.com',
+  databaseURL: 'https://lfgamers-4ade1.firebaseio.com',
+  projectId: "lfgamers-4ade1",
+  storageBucket: 'lfgamers-4ade1.appspot.com',
+  messagingSenderId: '928460245100'
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule,
+    // HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
